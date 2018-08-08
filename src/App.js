0bf5e42e893362fa9ae26ea7d65b1b08b2353e0e
Fernanda Roxo <fernandaroxo@gmail.com>
2018-08-08 16:47:48 +0100

    Put data in separate js file

--- src/App.js
+++ src/App.js
@@ -3,29 +3,12 @@ import './App.css';
 import BeatlesMap from "./BeatlesMap";
 import Sidebar from "./Sidebar";
 import escapeRegExp from 'escape-string-regexp';
+import data from './BeatlesSights';
 
 class App extends Component {
 
 	state = {
-		sights: [{
-			id: "paul_child",
-			name: "Paul's childhood home",
-			location: {lat: 53.3697068, lng: -2.900069},
-			foursquareID: "4c4854ad76d72d7f3b00404d",
-			keywords: ["mccartney", "macca", "fortlin road"]
-		},{
-			id: "john_child",
-			name: "John's childhood home",
-			location: {lat: 53.37727, lng: -2.8835431},
-			foursquareID: "4c1a117e95e89521e42557e7",
-			keywords: ["lennon", "mendips", "menlove avenue"]
-		},{
-			id: "penny",
-			name: "Penny Lane",
-			location: {lat: 53.3889678, lng: -2.9155062},
-			foursquareID: "4b9e5de8f964a5205fdd36e3",
-			keywords: ["roundabout", "nurse", "barber", "banker", "fireman"]
-		}],
+		sights: data.beatlesSights,
 		latitude: 53.397734,
 		longitude: -2.936724,
 		selectedSight: "",
