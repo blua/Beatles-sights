be09fc9d6d33b89050acad1257fde18a0ae44f0f
Fernanda Roxo <fernandaroxo@gmail.com>
2018-08-10 19:29:26 +0100

    Improve error catching

--- src/BeatlesMarker.js
+++ src/BeatlesMarker.js
@@ -20,13 +20,13 @@ export default class BeatlesMarker extends React.Component {
 				 { this.props.selected === this.props.sight === true ?
 					<InfoWindow maxWidth={800} defaultPosition={ this.props.location }>
 						<div>
-						{this.props.showInfo ?
+						{this.props.showInfo && this.props.data.response > 0 ?
 							<div>
 								<img src={"https://igx.4sqi.net/img/general/width960" + this.props.data.response.venue.bestPhoto.suffix} alt={this.props.data.response.venue.name} width="300px"/>
 								<br /><h3>{this.props.data.response.venue.name}</h3>
 								<a href={this.props.data.response.venue.canonicalUrl} target="_blank">Open on Foursquare</a>
 							</div> :
-								<span>Sorry, no info about <b>{this.props.sight.name}</b> available at the moment</span>
+								<span>Sorry, no info about <b>{this.props.sight.name}</b> available at the moment.</span>
 							}
 						</div>
 					</InfoWindow> : null
