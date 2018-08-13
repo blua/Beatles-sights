b577a6891fc01ceb3e7767aba7e5e452227393c1
Fernanda Roxo <fernandaroxo@gmail.com>
2018-08-13 17:56:58 +0100

    Add more sights

--- src/BeatlesSights.js
+++ src/BeatlesSights.js
@@ -4,19 +4,134 @@ const data =  { beatlesSights :
 		name: "Paul's childhood home",
 		location: {lat: 53.3697068, lng: -2.900069},
 		foursquareID: "4c4854ad76d72d7f3b00404d",
+		description: "The house where Paul lived with his parents Jim and Mary and his brother Michael.",
 		keywords: ["mccartney", "macca", "fortlin road"]
 	},{
 		id: "john_child",
 		name: "John's childhood home",
 		location: {lat: 53.37727, lng: -2.8835431},
 		foursquareID: "4c1a117e95e89521e42557e7",
+		description: "Mendips was the house where John was brought up by his Aunt Mimi and Uncle George.",
 		keywords: ["lennon", "mendips", "menlove avenue"]
 	},{
 		id: "penny",
 		name: "Penny Lane",
 		location: {lat: 53.3889678, lng: -2.9155062},
 		foursquareID: "4b9e5de8f964a5205fdd36e3",
+		description: "The famous street that became a number 1 song in 1967.",
 		keywords: ["roundabout", "nurse", "barber", "banker", "fireman"]
+	},{
+		id: "george_child",
+		name: "George's childhood home",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "4f1aeadde4b0ce22e4f1f0ae",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "ringo_child",
+		name: "",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "5022329e45b02dc7c5b1a96d",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "strawberry",
+		name: "Strawberry Field",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "cavern",
+		name: "Cavern Club",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "st_peter",
+		name: "St Peter's Church",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "Where John and Paul first met on July 6, 1957",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "eleanor_statue",
+		name: "Eleanor Rugby's statue",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "eleanor_grave",
+		name: "Eleanor Rugby's gravesite",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "mmt",
+		name: "Magical Mystery Tour starting point",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "story",
+		name: "The Beatles Story Museum",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "lpool_museum",
+		name: "Lierpool Museum",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "statue",
+		name: "Beatles Statue",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "institute",
+		name: "Liverpool Institute",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "Where Paul and George went to high school.",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "college_art",
+		name: "Liverpool College of Art",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "Where John went, with Stuart Stuart Sutcliffe.",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "casbah",
+		name: "Casbah Coffee Club",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "Caledonia",
+		name: "",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "Not Beatles-related, but it's my favorite Liverpool pub, so go check it out!",
+		keywords: ["", "", "", "", ""]
+	},{
+		id: "",
+		name: "",
+		location: {lat: 53.38000, lng: -2.900062},
+		foursquareID: "",
+		description: "",
+		keywords: ["", "", "", "", ""]
 	}]
 }
 
