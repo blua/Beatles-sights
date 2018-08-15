4ea7427e0c03be61010535528efeb6715a23fd03
Fernanda Roxo <fernandaroxo@gmail.com>
2018-08-15 09:17:59 +0100

    Add to the data

--- src/BeatlesSights.js
+++ src/BeatlesSights.js
@@ -5,56 +5,56 @@ const sights =  { beatlesSights :
 		location: {lat: 53.3697068, lng: -2.900069},
 		foursquareID: "4c4854ad76d72d7f3b00404d",
 		description: "20 Fortlin Road is the house where Paul lived with his parents Jim and Mary and his brother Michael from 1955 to 1963. Tours of the interior are available with the National Trust.",
-		keywords: ["mccartney"]
+		keywords: ["paul mccartney", "macca", "20 fortlin road"]
 	},{
 		id: "john_child",
 		name: "John's childhood home",
 		location: {lat: 53.37727, lng: -2.8835431},
 		foursquareID: "4c1a117e95e89521e42557e7",
 		description: "Located at 251 Menlove Avenue, Mendips is the house where John was brought up by his Aunt Mimi and Uncle George. Tours of the interior are available with the National Trust.",
-		keywords: ["lennon"]
+		keywords: ["john lennon", "mendips", "251 menlove avenue"]
 	},{
 		id: "penny",
 		name: "Penny Lane",
 		location: {lat: 53.3889678, lng: -2.9155062},
 		foursquareID: "4b9e5de8f964a5205fdd36e3",
 		description: "The famous street that became a number 1 song in 1967.",
-		keywords: ["roundabout", "nurse", "barber", "banker", "fireman"]
+		keywords: ["penny lane", "roundabout", "nurse", "barber", "banker", "fireman"]
 	},{
 		id: "george_child",
 		name: "George's childhood home",
 		location: {lat: 53.3989926,lng: -2.9188277},
 		foursquareID: "4f1aeadde4b0ce22e4f1f0ae",
-		description: "12 Arnold Grove is birthplace and childhood home of George Harrison.",
-		keywords: []
+		description: "12 Arnold Grove is the birthplace and childhood home of George Harrison.",
+		keywords: ["george harrison", "12 arnold grove"]
 	},{
 		id: "ringo_child",
 		name: "Ringo's childhood home",
 		location: {lat: 53.3887512, lng: -2.9618779},
 		foursquareID: "5022329e45b02dc7c5b1a96d",
 		description: "9 Madrin Street is the birthplace and ealy childhood home of Ringo Starr.",
-		keywords: ["starkey"]
+		keywords: ["ringo starr", "9 madrin street", "starkey"]
 	},{
 		id: "strawberry",
 		name: "Strawberry Field",
 		location: {lat: 53.3804178, lng: -2.8839224},
 		foursquareID: "4b5c794cf964a520823129e3",
 		description: "Strawberry Field, imortalized in the number 1 song Strawberry Fields Forever, was a Salvation Army children's home where John used to play with his friends.",
-		keywords: ["strawberry fields", "lennon", "gates", "beaconsfield road"]
+		keywords: ["strawberry field", "strawberry fields forever", "john lennon", "red gates", "beaconsfield road"]
 	},{
 		id: "cavern",
 		name: "The Cavern Club",
 		location: {lat: 53.4063747, lng: -2.9901746},
 		foursquareID: "4b927da0f964a520c2fd33e3",
 		description: "A faithful replica partially located at the sight of the original Cavern, where The Beatles started their rise to fame, having played over 250 shows there.",
-		keywords: ["cavern", "epstein", "club", "Mathew Street"]
+		keywords: ["cavern", "brian epstein", "Mathew Street", "concerts"]
 	},{
 		id: "st_peter",
 		name: "St. Peter's Church",
 		location: {lat: 53.375963, lng: -2.8715595},
 		foursquareID: "4d8f485e1d06b1f7df4e5b3b",
 		description: "Where John and Paul first met on July 6, 1957.",
-		keywords: ["peter", "saint", "church"]
+		keywords: ["john lennon", "paul mccartney", "woolton fete", "quarrymen"]
 	},{
 		id: "eleanor_statue",
 		name: "Eleanor Rigby's statue",
@@ -68,7 +68,7 @@ const sights =  { beatlesSights :
 		location: {lat: 53.3757795, lng: -2.8711187},
 		foursquareID: "4d1361030ad2f04dc700b354",
 		description: "A possible inspiration (never confirmed) for the song of the same name, Eleanor's grave is located in the graveyard of St. Peter's Church.",
-		keywords: ["eleanor rigby", "grave", "cemetery"]
+		keywords: ["eleanor rigby", "grave", "cemetery", "thombstone"]
 	},{
 		id: "mmt",
 		name: "Magical Mystery Tour starting point",
@@ -103,28 +103,28 @@ const sights =  { beatlesSights :
 		location: {lat: 53.399573, lng: -2.9744947},
 		foursquareID: "4b4a371ef964a5201c7f26e3",
 		description: "The building where Paul and George went to high school is now owned by Paul McCartney's art school, LIPA.",
-		keywords: ["liverpool", "institute", "school"]
+		keywords: ["liverpool institute", "high school", "paul mccartney", "george harrison"]
 	},{
 		id: "college_art",
 		name: "Liverpool College of Art",
 		location: {lat: 53.38000, lng: -2.900062},
 		foursquareID: "",
 		description: "The college where John went, with his first wife Cynthia and early Beatle Stuart Sutcliffe.",
-		keywords: []
+		keywords: ["college of art", "john lennon", "cynthia nixon", "cynthia lennon", "stuart sutcliffe"]
 	},{
 		id: "casbah",
 		name: "Casbah Coffee Club",
 		location: {lat: 53.4324354, lng: -2.9141807},
 		foursquareID: "4dc580b0e4cd169dc64e441e",
 		description: "The legendary music venue on the cellar of early Beatle Pete Best's home.",
-		keywords: ["casbah", "pete best", "club"]
+		keywords: ["casbah", "pete best", "club", "mona best", "concerts"]
 	},{
 		id: "caledonia",
 		name: "The Caledonia",
 		location: {lat: 53.4004987, lng: -2.9700728},
 		foursquareID: "4c7acdf981bca09313a80115",
 		description: "Not Beatles-related, but it's my favorite Liverpool pub, so go check it out!",
-		keywords: ["pub", "food", "drinks", "vegan"]
+		keywords: ["caledonia", "pub", "food", "drinks", "vegan", "dog friendly", "pet friendly"]
 	}]
 }
 
